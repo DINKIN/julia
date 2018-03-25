DINKIN/julia

commit e0aee185e60d8afbb53d93404ddede203d1ddfaf

a few more optimizations for package loading
ref #21173

src/typemap.c
@@ -641,15 +641,34 @@ static jl_typemap_entry_t *jl_typemap_assoc_by_type_(jl_typemap_entry_t *ml, jl_
     return NULL;
 }
 
+int jl_obviously_unequal(jl_value_t *a, jl_value_t *b);
+
 static jl_typemap_entry_t *jl_typemap_lookup_by_type_(jl_typemap_entry_t *ml, jl_tupletype_t *types, size_t world)
 {
     for (; ml != (void*)jl_nothing; ml = ml->next) {
         if (world < ml->min_world || world > ml->max_world)
             continue;
         // TODO: more efficient
-        if (jl_types_equal((jl_value_t*)types, (jl_value_t*)ml->sig)) {
-            return ml;
+        jl_value_t *a = (jl_value_t*)types;
+        jl_value_t *b = (jl_value_t*)ml->sig;
+        while (jl_is_unionall(a)) a = ((jl_unionall_t*)a)->body;
+        while (jl_is_unionall(b)) b = ((jl_unionall_t*)b)->body;
+        size_t na = jl_nparams(a), nb = jl_nparams(b);
+        assert(na > 0 && nb > 0);
+        if (!jl_is_vararg_type(jl_tparam(a,na-1)) && !jl_is_vararg_type(jl_tparam(b,nb-1))) {
+            if (na != nb)
+                continue;
         }
+        if (na > 1 && nb > 1) {
+            if (jl_obviously_unequal(jl_tparam(a,1), jl_tparam(b,1)))
+                continue;
+            if (na > 2 && nb > 2) {
+                if (jl_obviously_unequal(jl_tparam(a,2), jl_tparam(b,2)))
+                    continue;
+            }
+        }
+        if (jl_types_equal((jl_value_t*)types, (jl_value_t*)ml->sig))
+            return ml;
     }
     return NULL;
 }

src/subtype.c
@@ -239,6 +239,11 @@ static int obviously_unequal(jl_value_t *a, jl_value_t *b)
     return 0;
 }
 
+int jl_obviously_unequal(jl_value_t *a, jl_value_t *b)
+{
+    return obviously_unequal(a, b);
+}
+
 static int obviously_disjoint(jl_value_t *a, jl_value_t *b, int specificity)
 {
     if (a == b || a == (jl_value_t*)jl_any_type || b == (jl_value_t*)jl_any_type)
@@ -542,8 +547,9 @@ static int subtype_unionall(jl_value_t *t, jl_unionall_t *u, jl_stenv_t *e, int8
     // in the environment, rename to get a fresh var.
     while (btemp != NULL) {
         if (btemp->var == u->var ||
-            (btemp->lb != jl_bottom_type && jl_has_typevar(btemp->lb, u->var)) ||
-            (btemp->ub != (jl_value_t*)jl_any_type && jl_has_typevar(btemp->ub, u->var))) {
+            // outer var can only refer to inner var if bounds changed
+            (btemp->lb != btemp->var->lb && jl_has_typevar(btemp->lb, u->var)) ||
+            (btemp->ub != btemp->var->ub && jl_has_typevar(btemp->ub, u->var))) {
             u = rename_unionall(u);
             break;
         }
@@ -627,7 +633,8 @@ static int subtype_unionall(jl_value_t *t, jl_unionall_t *u, jl_stenv_t *e, int8
     btemp = e->vars;
     while (btemp != NULL) {
         jl_value_t *vi = btemp->ub;
-        if (vi != (jl_value_t*)vb.var && vi != (jl_value_t*)jl_any_type && jl_has_typevar(vi, vb.var)) {
+        // TODO: this takes a significant amount of time
+        if (vi != (jl_value_t*)vb.var && btemp->var->ub != vi && jl_has_typevar(vi, vb.var)) {
             btemp->ub = jl_new_struct(jl_unionall_type, vb.var, vi);
             btemp->lb = jl_bottom_type;
         }
@@ -692,11 +699,11 @@ static int subtype_tuple(jl_datatype_t *xd, jl_datatype_t *yd, jl_stenv_t *e, in
     jl_value_t *lastx=NULL, *lasty=NULL;
     while (i < lx) {
         jl_value_t *xi = jl_tparam(xd, i);
-        if (jl_is_vararg_type(xi)) vx = 1;
+        if (i == lx-1 && vvx) vx = 1;
         jl_value_t *yi = NULL;
         if (j < ly) {
             yi = jl_tparam(yd, j);
-            if (jl_is_vararg_type(yi)) vy = 1;
+            if (j == ly-1 && vvy) vy = 1;
         }
         if (vx && !vy) {
             if (!check_vararg_length(xi, ly+1-lx, e))
@@ -892,9 +899,9 @@ static int subtype(jl_value_t *x, jl_value_t *y, jl_stenv_t *e, int param)
             xd = xd->super;
         }
         if (xd == jl_any_type) return 0;
-        if (jl_is_tuple_type(xd))
+        if (xd->name == jl_tuple_typename)
             return subtype_tuple(xd, yd, e, param);
-        if (jl_is_vararg_type((jl_value_t*)xd)) {
+        if (xd->name == jl_vararg_typename) {
             // Vararg: covariant in first parameter, invariant in second
             jl_value_t *xp1=jl_tparam0(xd), *xp2=jl_tparam1(xd), *yp1=jl_tparam0(yd), *yp2=jl_tparam1(yd);
             // in Vararg{T1} <: Vararg{T2}, need to check subtype twice to

commit e9918042d2d267887ddf3469944258adddffbda8

typemap: fix bug with handling of Tuple{}
introduced by e0aee185e60d8afbb53d93404ddede203d1ddfaf

src/typemap.c
@@ -626,21 +626,21 @@ static jl_typemap_entry_t *jl_typemap_lookup_by_type_(jl_typemap_entry_t *ml, jl
         if (world < ml->min_world || world > (ml->max_world | max_world_mask))
             continue;
         // TODO: more efficient
-        jl_value_t *a = types;
-        jl_value_t *b = (jl_value_t*)ml->sig;
-        while (jl_is_unionall(a)) a = ((jl_unionall_t*)a)->body;
-        while (jl_is_unionall(b)) b = ((jl_unionall_t*)b)->body;
-        size_t na = jl_nparams(a), nb = jl_nparams(b);
-        assert(na > 0 && nb > 0);
-        if (!jl_is_vararg_type(jl_tparam(a,na-1)) && !jl_is_vararg_type(jl_tparam(b,nb-1))) {
+        jl_value_t *a = jl_unwrap_unionall(types);
+        jl_value_t *b = jl_unwrap_unionall((jl_value_t*)ml->sig);
+        size_t na = jl_nparams(a);
+        size_t nb = jl_nparams(b);
+        int va_a = na > 0 && jl_is_vararg_type(jl_tparam(a, na - 1));
+        int va_b = nb > 0 && jl_is_vararg_type(jl_tparam(b, nb - 1));
+        if (!va_a && !va_b) {
             if (na != nb)
                 continue;
         }
-        if (na > 1 && nb > 1) {
-            if (jl_obviously_unequal(jl_tparam(a,1), jl_tparam(b,1)))
+        if (na >= (1 - va_a) && nb > (1 - va_b)) {
+            if (jl_obviously_unequal(jl_tparam(a, 1), jl_tparam(b, 1)))
                 continue;
-            if (na > 2 && nb > 2) {
-                if (jl_obviously_unequal(jl_tparam(a,2), jl_tparam(b,2)))
+            if (na > (2 - va_a) && nb > (2 - va_b)) {
+                if (jl_obviously_unequal(jl_tparam(a, 2), jl_tparam(b, 2)))
                     continue;
             }
         }